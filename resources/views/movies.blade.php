bdab6cce714f0fcb720753c8fc9c03a067cbd089
moonymel <musumeci.me@gmail.com>
2024-02-14 16:41:55 +0100

    [UPDATE] updating infos in movies.blade.php

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,8 +2,29 @@
 
 @section('content')
 
-<main class="bg-light">
-    aiutooo questa è una prova dimmi che funziona y_y
+<main>
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-12 text-center">
+                <h1>Film disponibili</h1>
+            </div>
+            @foreach ($movies as $movie)
+                <div class="col-3 my-3">
+                    <div class="card">
+                        <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie->original_title }}</h6>
+                        <p class="card-text">
+                            data di rilascio: {{ $movie->date }}<br>
+                            nazionalità: {{ $movie->nationality }}<br>
+                            voto: {{ $movie->vote }}<br>
+                        </p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </main>
 
 @endsection
